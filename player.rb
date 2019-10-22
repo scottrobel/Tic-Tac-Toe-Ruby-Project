c24393673bd8ea550f6fcd81e8856bc062178064
scottrobel <robelrain@gmail.com>
2019-10-22 01:13:05 -0400

    Added board game and player files

--- player.rb
+++ player.rb
@@ -4,6 +4,5 @@ class Player
   def initialize(player_name)
     @player_name = player_name
     @x_or_o = @@avalible_player_options.pop() if(!@@avalible_player_options.empty?)
-    #puts "Hello #{self.player_name} You are #{@x_or_o}'s"
   end
 end
